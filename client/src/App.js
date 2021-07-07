9c4cb4af8a2d863d37b36351144bf2551e25ca03
hcalhoun <32774710+hcalhoun@users.noreply.github.com>
2021-07-07 14:31:14 -0400

    updated yaml

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,19 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
-        </div>
+        </header>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
